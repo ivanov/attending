9033498dd729df2fe6a4e8a36013a0d59bbc3c56
Paul Ivanov <pivanov5@bloomberg.net>
2019-09-06 12:15:54 -0700

    python 3.6 supported

--- setup.py
+++ setup.py
@@ -104,6 +104,7 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         # These classifiers are *not* checked by 'pip install'. See instead
         # 'python_requires' below.
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
@@ -131,7 +132,7 @@ setup(
     # and refuse to install the project if the version does not match. If you
     # do not support Python 2, you can simplify this to '>=3.5' or similar, see
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
-    python_requires='>=3.7, <4',
+    python_requires='>=3.6, <4',
 
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
