1b73815177a0ac1cbb2e471b74927cb8505c95de
John Purviance <jpurviance@vt.edu>
2019-09-07 15:04:58 -0700

    fix up examples

--- attending/__init__.py
+++ attending/__init__.py
@@ -1,1 +1,1 @@
-from .library import Library, attending_doc, fetch, fetch_via_module, fetch_via_name, fetch_via_local_index
+from .library import Library, attending_doc, fetch, fetch_via_module, fetch_via_name, fetch_via_local_index, contains
